4fdf203e71ba0f44bcb1f9b6c510b0b66c7a500c
franklinpinte <franklinpinte@outlook.com>
2025-09-17 22:51:01 -0600

    Reapply "Entidades de todas las tablas en Entities y Repositories"
    This reverts commit 13c24d04b8035a909a88e39d9ace473354c4a801.

---
+++ LaboratorioClinico.Domain/Repositories/IExamenRepository.cs
@@ -0,0 +1,27 @@
+﻿using LaboratorioClinico.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioClinico.Domain.Repositories
+{
+    public interface IExamenRepository
+    {
+        // Obtener todos los examenes
+        Task<IEnumerable<Examen>> GetExamenesAsync();
+
+        // Obtener un examen por su id
+        Task<Examen> GetExamenByIdAsync(int id);
+
+        // Agregar un nuevo examen
+        Task<Examen> AddExamenAsync(Examen examen);
+
+        // Actualizar un examen existente
+        Task<Examen> UpdateExamenAsync(Examen examen);
+
+        // Eliminar un examen por su id
+        Task<bool> DeleteExamenAsync(int id);
+    }
+}
---
+++ LaboratorioClinico.Domain/Repositories/IRolRepository.cs
@@ -0,0 +1,27 @@
+﻿using LaboratorioClinico.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioClinico.Domain.Repositories
+{
+    public interface IRolRepository
+    {
+        // Obtener todos los roles
+        Task<IEnumerable<Rol>> GetRolesAsync();
+
+        // Obtener un rol por su id
+        Task<Rol> GetRolByIdAsync(int id);
+
+        // Agregar un nuevo rol
+        Task<Rol> AddRolAsync(Rol rol);
+
+        // Actualizar un rol existente
+        Task<Rol> UpdateRolAsync(Rol rol);
+
+        // Eliminar un rol por su id
+        Task<bool> DeleteRolAsync(int id);
+    }
+}
