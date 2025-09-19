269870b99a947be8de393abd529e1e8a9d5567bb
Teresa Ayala <teresaayala17@outlook.com>
2025-09-18 23:33:48 -0600

    Correcciones en la LaboratorioClinico.WebAPI

---
+++ LaboratorioClinico.WebAPI/Controllers/DoctorController.cs
@@ -0,0 +1,52 @@
+﻿using LaboratorioClinico.Domain.Entities;
+using LaboratorioClinico.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LaboratorioClinico.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public DoctorController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Doctor/get
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Doctor>>> Get()
+        {
+            return await _context.Doctores.ToListAsync();
+        }
+
+        // GET api/<DoctorController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DoctorController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DoctorController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DoctorController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
