942a347a0ee6da6d0ecae3f8962f365907430b6a
franklinpinte <franklinpinte@outlook.com>
2025-10-15 19:49:13 -0600

    Se creo la prueba con Selenium. UIWeb, agregando paquetes NuGet, 5 en total. La prueba se realizo y se aplico con exito.

---
+++ PruebaUIWeb/GoogleSearchTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Referencias
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace PruebaUIWeb
+{
+    [TestClass]
+    public class GoogleSearchTest
+    {
+        private IWebDriver driver;
+
+        public GoogleSearchTest()
+        {
+            //Inicializa el controlador de Chrome
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void SearchInGoogle()
+        {
+            // Navega a Google
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            // Encuentra el cuadro de búsqueda
+            var searchBox = driver.FindElement(By.Name("q"));
+
+            // Ingresa la búsqueda
+            searchBox.SendKeys("OpenAI");
+
+            // Enviar la búsqueda
+            searchBox.Submit();
+
+            // Espera un momento para que se carguen los resultados
+            System.Threading.Thread.Sleep(2000);
+
+            // Verifica que el título de la página contenga "OpenAI"
+            Assert.IsTrue(driver.Title.Contains("OpenAI"), "El título no contiene 'OpenAI'");
+        }
+
+
+    public void Dispose()
+        {
+            // Cierra el navegador
+            driver.Quit();
+        }
+    
+    }
+}
